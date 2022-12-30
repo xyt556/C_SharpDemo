0e58842119340ed0401e8a1c1734a6be29cdd02e
Yantao XI <xyt556@gmail.com>
2022-12-30 17:18:38 +0800

    数组

---
+++ 数组求和/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace cal
+{
+    class Program
+    {
+        const int M = 3, N = 4; //定义常量
+        static void Main(string[] args)
+        {
+            int[,] a = new int[,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+            int[,] b = new int[,] { { 1, 4, 7, 10 }, { 2, 5, 8, 11 }, { 3, 6, 9, 12 } };
+            int[,] c = new int[M, N]; //创建M行N列二维数组c
+                                      //矩阵求和
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                    c[i, j] = a[i, j] + b[i, j];
+            //按M行N列输出矩阵a,每个数据占4位宽度
+            Console.WriteLine("矩阵a是：");
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    Console.Write("{0,4}", a[i, j]);
+                Console.WriteLine();
+            }
+            //按M行N列输出矩阵b,每个数据占4位宽度
+            Console.WriteLine("矩阵b是：");
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                    Console.Write("{0,4}", b[i, j]);
+                Console.WriteLine();
+            }
+            //按M行N列输出矩阵c,每个数据占4位宽度
+            Console.WriteLine("结果矩阵是：");
+            for (int i = 0; i < c.GetLength(0); i++)
+            {
+                for (int j = 0; j < c.GetLength(1); j++)
+                    Console.Write("{0,4}", c[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 杨辉三角/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace yh
+{
+    class Yh
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入行数");
+            int rows = int.Parse(Console.ReadLine());
+            //初始化二维数组
+            int[][] nums = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                nums[i] = new int[i + 1];
+            }
+            //边界赋值
+            for (int i = 0; i < rows; i++)
+            {
+                nums[i][0] = 1;
+                nums[i][i] = 1;
+            }
+            //中心值赋值
+            for (int i = 2; i < rows; i++) //控制行数
+            {
+                for (int j = 1; j < i; j++) //控制列数
+                {
+                    nums[i][j] = nums[i - 1][j] + nums[i - 1][j - 1]; //每一列的值等于上一行的列+上一行的列-1的值
+                }
+            }
+            //输出
+            for (int i = 0; i < rows; i++)
+            {
+                //打印空格
+                for (int k = 0; k < rows - i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(nums[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
