d7b73a5b2a4818fa1a9fea508ca9f312fb450a78
Yantao XI <xyt556@gmail.com>
2022-12-31 09:02:03 +0800

    zywt

--- 作业问题/Program.cs
+++ 作业问题/Program.cs
@@ -1,15 +1,10 @@
 ﻿/*
 Title: 作业问题
-
 Description: 小霖原计划每天写M小时作业，N天可以完成。现在由于有其他安排，需要将作业提前X天完成，请问她每天写Y小时作业可以完成么？
-
 Input: 输入M,N,X,Y四个正整数
-
 Output: 输出“yes”或“no”，分别代表能够完成或不能够完成
-
 sample_input:
 3 15 5 4
-
 sample_output:
 no*/
 using System;
