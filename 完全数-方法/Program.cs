c7c2de36e4420986eca53e076d40e361dde1a9d9
Yantao XI <xyt556@gmail.com>
2023-01-01 16:20:36 +0800

    完全数

--- 完全数-方法/Program.cs
+++ 完全数-方法/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class PerfectNum
 {
-    static bool perfect(int n)
+    static bool perfect(int n)  // 定义方法
     {
         int sum = 0;
         for (int i = 1; i < n; i++)//计算n的所有真因子之和
@@ -16,6 +16,6 @@ class PerfectNum
         int n;
         Console.WriteLine("请输入要判别的数：");
         n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("{0}是完全数吗？{1}", n, perfect(n));
+        Console.WriteLine("{0}是完全数吗？{1}", n, perfect(n)); //引用方法
     }
 }
