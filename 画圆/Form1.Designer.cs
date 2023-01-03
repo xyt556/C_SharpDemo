c05df796a4034bebd4f7509e277ddc6955188727
Yantao XI <xyt556@gmail.com>
2023-01-03 10:29:59 +0800

    画圆

--- 画圆/Form1.Designer.cs
+++ 画圆/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -60,11 +61,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "写文字";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -80,5 +92,6 @@
         private Label label1;
         private TextBox textBox1;
         private Button button1;
+        private Button button2;
     }
 }
--- 画圆/Form1.cs
+++ 画圆/Form1.cs
@@ -17,6 +17,15 @@ namespace 画圆
             //设置圆的填充色为蓝色
             gra.FillEllipse(bush, 0, 0, 2 * r, 2 * r);//画圆（//画填充椭圆的方法，x坐标、y坐标、宽、高，如果是100，则半径为50）
             gra.DrawEllipse(pen, 0, 0, 2 * r, 2 * r);
+            gra.DrawEllipse(pen, 250, 10, 100, 100);//画椭圆的方法，x坐标、y坐标、宽、高，如果是100，则半径为50
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics gra = this.CreateGraphics();
+            Font myFont = new Font("宋体", 60, FontStyle.Bold);
+            Brush bush = new SolidBrush(Color.Red);//填充的颜色
+            gra.DrawString("欢迎！", myFont, bush, 300, 300);
         }
     }
 }
