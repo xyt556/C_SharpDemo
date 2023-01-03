dfaf4f26341ddc03caf4fddb616aa18e77f85d1f
Yantao XI <xyt556@gmail.com>
2023-01-03 10:26:48 +0800

    画圆

--- 画圆/Form1.Designer.cs
+++ 画圆/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 406);
+            this.label1.Location = new System.Drawing.Point(188, 418);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 17);
             this.label1.TabIndex = 0;
@@ -44,14 +44,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 403);
+            this.textBox1.Location = new System.Drawing.Point(276, 415);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(311, 403);
+            this.button1.Location = new System.Drawing.Point(408, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
--- 画圆/Form1.cs
+++ 画圆/Form1.cs
@@ -13,10 +13,10 @@ namespace 画圆
             r = Convert.ToInt32(textBox1.Text);//取半径值
             Graphics gra = this.CreateGraphics();
             Brush bush = new SolidBrush(Color.Blue);
+            Pen pen = new Pen(Color.Pink);//画笔颜色    
             //设置圆的填充色为蓝色
-            
-            gra.FillEllipse(bush, 0, 0, 2 * r, 2 * r);//画圆
-            
+            gra.FillEllipse(bush, 0, 0, 2 * r, 2 * r);//画圆（//画填充椭圆的方法，x坐标、y坐标、宽、高，如果是100，则半径为50）
+            gra.DrawEllipse(pen, 0, 0, 2 * r, 2 * r);
         }
     }
 }
