9c42fda6d6a4520c079d432f5ce42d899cfbd555
Yantao XI <xyt556@gmail.com>
2023-01-03 17:13:21 +0800

    求最大公约数和最小公倍数

--- 最大公约数最大公倍数-窗体/Form1.Designer.cs
+++ 最大公约数最大公倍数-窗体/Form1.Designer.cs
@@ -145,8 +145,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "求最大公约数和最小公倍数";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
