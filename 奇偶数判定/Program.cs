4a44bdcb1b6270c249f74fd913cd0598ea4c284a
Yantao XI <xyt556@gmail.com>
2023-01-06 14:15:14 +0800

    奇偶数判别

---
+++ 奇偶数判定/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class pro
+{
+    static void Main(string[] args)
+    {
+        int x;
+
+        Console.WriteLine("请输入一个整数：");
+        x = Convert.ToInt16(Console.ReadLine());
+        if (x%2==0)
+        {
+            Console.WriteLine("你输入的数是偶数！");
+        }
+        else
+        {
+            Console.WriteLine("你输入的数是奇数！");
+        }
+    }
+}
