6f46d38f29455d2c7b32553c06bc5ec11b68d1f0
Yantao XI <xyt556@gmail.com>
2023-01-07 15:14:21 +0800

    test

---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿class MyClass
+{
+    static void Main(string[] args)
+    {
+        String str = "大家" + '\u0022' + "好" + '\'';
+
+        Console.WriteLine(str);
+
+        Console.ReadLine();
+    }
+}
