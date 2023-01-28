62ceb501dac2ffb8765a5bb4dd73243796925ad4
Yantao XI <xyt556@gmail.com>
2023-01-28 10:01:52 +0800

    一个简单类的示例

---
+++ 一个简单类的示例/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Hello  //声明一个类-Hello
+{
+    private int x = 1;  // x 为字段名
+    public void DisplayMessage()    //声明一个方法
+    {
+        Console.WriteLine("Hello");
+    }
+    static void Main(string[] args)
+    {
+        Hello myHello = new Hello(); //类并不能直接使用，需要根据类来创建对象，也称为类的实例化。通过new关键字，创建了Hello类的一个对象myHello。
+        myHello.DisplayMessage(); //调用Hello类中的DisplayMessage方法。
+    }
+}
