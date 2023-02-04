52a2c3c7ba17b3653d3f0d3689e64d3f32b9e311
Yantao XI <xyt556@gmail.com>
2023-02-04 14:52:29 +0800

    成绩排序

---
+++ 成绩排序/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace 成绩排序
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学生成绩：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(104, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(123, 208);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "排序结果：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 358);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "成绩排序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ 成绩排序/Form1.cs
@@ -0,0 +1,87 @@
+namespace 成绩排序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;        //接收待排序的输入数据
+            int[] int_Array;   //存储以前输入的排好序的数据
+            int i;  //临时变量
+            //分别存储折半查找时的找到的位置、起点、终点和中间点
+            int foundPosition = 0, StartPosition = 0, endPositon = 0,midPosition=0;
+            bool found;  //存储是否找到需要查找的数据，0表示未找到，1表示找到
+            str = textBox1.Text.Trim();//接收戴插入排序的输入数据（去除空格）
+            if (str.Length == 0)
+            {
+                MessageBox.Show("请输入戴处理的数据！");
+                return;
+            }
+            //判断待处理的数据是否为数值
+            for (i = 0; i < str.Length; i++)
+            {
+                if (!char.IsNumber(str[i]))
+                {
+                    MessageBox.Show("待处理的数据中有非数值存在！");
+                    return;
+                }
+            }
+            //数组实例化
+            int_Array= new int[listBox1.Items.Count];
+            //将以前输入并排序好的数据转到数组中
+            for (i = 0; i < listBox1.Items.Count; i++)
+            {
+                int_Array[i] = Int32.Parse(listBox1.Items[i].ToString());
+            }
+            //折半查找法定位数据应该在数组中的位置
+            if (listBox1.Items.Count == 0)
+            {
+                foundPosition = 0;//第一次插入数据时，不需寻找，直接插入到第一个位置
+            }
+            else
+            {
+                StartPosition = 0;
+                endPositon = int_Array.Length - 1;
+                found= false;
+                //循环查找数组中是否有要插入的数据
+                while (!found && StartPosition<=endPositon)
+                {
+                    midPosition = (StartPosition + endPositon) / 2;//取中间值
+                    if (int_Array[midPosition].Equals(Int32.Parse(str)))
+                    {
+                        found = true;
+                        foundPosition= midPosition;
+                    }
+                    else
+                    {
+                        if (Int32.Parse(str).CompareTo(int_Array[midPosition])<0)
+                        {
+                            endPositon = midPosition - 1;
+                        }
+                        //要插入的数据只能在左边
+                        else
+                        {
+                            StartPosition= midPosition+1;
+                        }
+                        //要插入的数据只能在右边
+                    }
+                    //未找到，则修改返回点
+                    if (!found)
+                    {
+                        foundPosition = StartPosition;
+                    }
+                }
+            }
+            listBox1.Items.Insert(foundPosition, Int32.Parse(str));
+        }
+    }
+}
