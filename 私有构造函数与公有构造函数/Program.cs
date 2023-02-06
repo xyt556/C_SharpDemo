401224caa686f6713ca8f64793f1740d4c2db40b
Yantao XI <xyt556@gmail.com>
2023-02-06 09:52:25 +0800

    私有构造函数与公有构造函数

---
+++ 私有构造函数与公有构造函数/Program.cs
@@ -0,0 +1,19 @@
+﻿public class privateTest
+{
+    private privateTest()//私有构造函数
+    {
+        Console.WriteLine("private");
+    }
+    public privateTest(string a) //公有构造函数
+    {
+        Console.WriteLine("public：{0}", a);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        //privateTest f1 = new privateTest();//不能实例化
+        privateTest f2 = new privateTest("ww");//调用公有构造函数实例化
+    }
+}
