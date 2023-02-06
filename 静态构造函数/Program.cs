04a53c82a9897e6a999c55828f89923ea6bf1f8e
Yantao XI <xyt556@gmail.com>
2023-02-06 09:43:30 +0800

    静态构造函数

---
+++ 静态构造函数/Program.cs
@@ -0,0 +1,20 @@
+﻿class BaseClass
+{
+    private static int CallCounter;
+    static BaseClass()
+    {
+        CallCounter = 2;
+        Console.WriteLine("Static CallCounter{0}", CallCounter);
+    }
+    public BaseClass()
+    {
+        CallCounter++;
+        Console.WriteLine(" CallCounter{0}", CallCounter);
+    }
+    static void Main(string[] args)
+    {
+        BaseClass b1 = new BaseClass();
+        BaseClass b2 = new BaseClass();
+        BaseClass b3 = new BaseClass();
+    }
+}
