c2c152d9d3ad695937f0feb66a2e2be99b0f8741
Yantao XI <xyt556@gmail.com>
2023-02-06 09:55:04 +0800

    保护型构造函数

---
+++ 保护型构造函数/Program.cs
@@ -0,0 +1,19 @@
+﻿public class protectTest
+{
+    protected protectTest()
+    {
+        Console.WriteLine("protect");
+    }
+    public protectTest(string a)
+    {
+        Console.WriteLine("public：{0}",a);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        //protectTest f = new protectTest();//不能实例化
+        protectTest f1 = new protectTest("ww");//调用公有构造函数实例化
+    }
+}
