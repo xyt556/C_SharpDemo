0e55a013a7a2a2e960619296ffa2a2ae9cb846df
Yantao XI <xyt556@gmail.com>
2023-02-07 15:50:21 +0800

    可空变量

---
+++ 可空变量/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace xyt
+{
+    class pro
+    {
+        static void Main(string[] args)
+        {
+            //int? i;
+            //double? d1 = 3.14;
+            //bool? flag = null;
+            //char? letter = 'a';
+            //使用可空类型变量时，C#提供了两/*种方法测试变量值是否为null以避免变量的错误引用。
+            //方法1
+            int? num = null;
+            if (num.HasValue == true)
+            {
+                Console.WriteLine("num = " + num.Value);
+
+            }
+            else
+            {
+                Console.WriteLine("num = null");
+            }
+            //方法2
+            int? y = 10;
+            if (y !=null)
+            {
+                Console.WriteLine("y = " + y.Value);
+
+            }
+            else
+            {
+                Console.WriteLine("y = null");
+            }
+        }
+    }
+        
+}
