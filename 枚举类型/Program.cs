250f16320776656f2fcb1e30c7c9219b1caad4b4
Yantao XI <xyt556@gmail.com>
2023-02-07 13:15:55 +0800

    枚举类型

---
+++ 枚举类型/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace test
+{
+    public enum TimeofDay  //枚举类型定义
+    {
+        Morning=1,
+        Afternoon,
+        Evening
+    }
+    class Pro
+    {
+        static void WriteGreeting(TimeofDay timeofDay)  //静态方法
+        {
+            switch (timeofDay)
+            {
+                case TimeofDay.Morning:
+                    Console.WriteLine("Good Morning!");
+                    break;
+                case TimeofDay.Afternoon:
+                    Console.WriteLine("Good Afternoon!");
+                    break;
+                case TimeofDay.Evening:
+                    Console.WriteLine("Good Evening!");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteGreeting(TimeofDay.Morning);
+            WriteGreeting(TimeofDay.Afternoon);
+            WriteGreeting(TimeofDay.Evening);
+        }
+    }
+}
