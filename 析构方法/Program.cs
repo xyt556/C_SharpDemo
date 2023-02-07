f2d930794b3747dbcfa8829d1347ed05e3ba0e89
Yantao XI <xyt556@gmail.com>
2023-02-07 14:54:20 +0800

    析构函数

--- 析构方法/Program.cs
+++ 析构方法/Program.cs
@@ -19,6 +19,11 @@
         {
             A x = new A(3);
             A y = new A(5);
+            string s1,s2,result;//定义三个变量
+            s1 = Console.ReadLine();//输入第一个字符串给s1
+            s2 = Console.ReadLine();//输入第二个字符串给s2
+            result = s1 + " "+s2;//对两个字符串进行首尾相接后给result
+            Console.WriteLine("{0}+{1}={2}",s1,s2,result);//输出
         }
     }
 }
