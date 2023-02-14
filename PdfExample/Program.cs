a3344574383eba2f401f770ccb613cde4b06cc15
Yantao XI <xyt556@gmail.com>
2023-02-14 15:53:11 +0800

    PdfExample

---
+++ PdfExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个Document对象
+            Document document = new Document();
+
+            // 创建一个PdfWriter对象
+            PdfWriter.GetInstance(document, new FileStream(@"C:\example.pdf", FileMode.Create));
+
+            // 打开文档
+            document.Open();
+
+            // 在文档中添加一些文字
+            BaseFont baseFont = BaseFont.CreateFont("C:\\Windows\\Fonts\\simhei.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont, 12, Font.NORMAL);
+            document.Add(new Paragraph("这是一个支持中文的PDF文档", font));
+
+            // 关闭文档
+            document.Close();
+        }
+    }
+}
