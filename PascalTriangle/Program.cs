e73660896b824a1785080f2cb5fd77ddf8624aea
Yantao XI <xyt556@gmail.com>
2023-02-14 13:54:39 +0800

    VectorDataCreationExample

---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入杨辉三角形的行数：");
+            int n = int.Parse(Console.ReadLine());
+            int[][] pascalTriangle = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                pascalTriangle[i] = new int[i + 1];
+                pascalTriangle[i][0] = 1;
+                pascalTriangle[i][i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(pascalTriangle[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VectorDataCreationExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using OSGeo.OGR;
+
+namespace VectorDataCreationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 初始化 OGR
+            Ogr.RegisterAll();
+
+            // 创建数据源
+            var dataSource = Ogr.Open("vector_data.shp", 1);
+            if (dataSource == null)
+            {
+                dataSource = Ogr.CreateDataSource("vector_data.shp", null);
+            }
+
+            // 创建图层
+            var layer = dataSource.CreateLayer("layer_name", null, wkbGeometryType.wkbPoint, null);
+
+            // 创建字段
+            var fieldDefinition = new FieldDefn("field_name", FieldType.OFTString);
+            fieldDefinition.SetWidth(32);
+            layer.CreateField(fieldDefinition, 1);
+
+            // 创建要素
+            var feature = new Feature(layer.GetLayerDefn());
+            feature.SetField(0, "feature_name");
+
+            // 创建几何形状
+            var point = new Geometry(wkbGeometryType.wkbPoint);
+            point.AddPoint(10, 10, 0);
+            feature.SetGeometry(point);
+
+            // 把要素写入图层
+            layer.CreateFeature(feature);
+
+            // 关闭数据源
+            dataSource.Dispose();
+        }
+    }
+}
