343b04cf8b249a0b71b2df626ad9483d865131e0
xyt556 <xyt556@gmail.com>
2023-02-21 14:34:51 +0800

    enum_demo

--- enum_demo/Program.cs
+++ enum_demo/Program.cs
@@ -10,7 +10,7 @@ namespace enum_demo
     {
         enum DaysOfWeek
         {
-            Monday,
+            Monday = 1,
             Tuesday,
             Wednesday,
             Thursday,
@@ -18,45 +18,70 @@ namespace enum_demo
             Saturday,
             Sunday
         }
+        public enum TimeofDay //枚举类型定义 06：	
+        {
 
+            Morning = 1,
+            Afternoon,
+            Evening
+        }
+        static void WriteGreeting(TimeofDay timeofDay)  //静态方法
+        {
+            switch (timeofDay)
+            {
+                case TimeofDay.Morning:
+                    Console.WriteLine("Hi, goodmorning"); break;
+                case TimeofDay.Afternoon: 
+                    Console.WriteLine("Hi, goodafternoon"); break;
+                case TimeofDay.Evening: 
+                    Console.WriteLine("Hi, goodevening"); break;
+            }
+        }
         public static void Main()
         {
-            // 使用枚举类型变量
-            DaysOfWeek today =  DaysOfWeek.Monday;
+            // 使用枚举类型变量DaysOfWeek
+            DaysOfWeek today = DaysOfWeek.Wednesday;
             Console.WriteLine("Today is {0}", today);
-
             // 枚举类型变量可以转换为整数
             int dayNumber = (int)today;
             Console.WriteLine("The day number for {0} is {1}", today, dayNumber);
+            // 使用枚举类型变量timeofDay
+            WriteGreeting(TimeofDay.Morning);
+            WriteGreeting(TimeofDay.Evening);
+            WriteGreeting(TimeofDay.Afternoon);
 
-            // 使用枚举类型的值作为 switch 语句的条件
-            switch (today)
-            {
-                case DaysOfWeek.Monday:
-                    Console.WriteLine("Today is Monday");
-                    break;
-                case DaysOfWeek.Tuesday:
-                    Console.WriteLine("Today is Tuesday");
-                    break;
-                case DaysOfWeek.Wednesday:
-                    Console.WriteLine("Today is Wednesday");
-                    break;
-                case DaysOfWeek.Thursday:
-                    Console.WriteLine("Today is Thursday");
-                    break;
-                case DaysOfWeek.Friday:
-                    Console.WriteLine("Today is Friday");
-                    break;
-                case DaysOfWeek.Saturday:
-                    Console.WriteLine("Today is Saturday");
-                    break;
-                case DaysOfWeek.Sunday:
-                    Console.WriteLine("Today is Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Invalid day");
-                    break;
-            }
         }
     }
 }
+
+
+
+
+// 使用枚举类型的值作为 switch 语句的条件
+//switch (today)
+//{
+//    case DaysOfWeek.Monday:
+//        Console.WriteLine("Today is Monday");
+//        break;
+//    case DaysOfWeek.Tuesday:
+//        Console.WriteLine("Today is Tuesday");
+//        break;
+//    case DaysOfWeek.Wednesday:
+//        Console.WriteLine("Today is Wednesday");
+//        break;
+//    case DaysOfWeek.Thursday:
+//        Console.WriteLine("Today is Thursday");
+//        break;
+//    case DaysOfWeek.Friday:
+//        Console.WriteLine("Today is Friday");
+//        break;
+//    case DaysOfWeek.Saturday:
+//        Console.WriteLine("Today is Saturday");
+//        break;
+//    case DaysOfWeek.Sunday:
+//        Console.WriteLine("Today is Sunday");
+//        break;
+//    default:
+//        Console.WriteLine("Invalid day");
+//        break;
+//}
