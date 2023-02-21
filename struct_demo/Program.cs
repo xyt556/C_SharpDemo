d23fad48cf6cb60028634535d9f7158fb03b03c8
xyt556 <xyt556@gmail.com>
2023-02-21 15:09:43 +0800

    struct

---
+++ struct_demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_demo
+{
+    struct Person
+    {
+        public string Name;
+        public int Age;
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", Name, Age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个 Person 实例
+            Person person = new Person();
+            person.Name = "Alice";
+            person.Age = 30;
+            // 调用实例方法
+            person.Display();
+            // 修改实例字段的值
+            person.Age = 31;
+            // 调用实例方法
+            person.Display();
+        }
+    }
+}
