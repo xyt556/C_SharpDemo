a7c64e2bca1a3b946cf0c200b0886428cd0c00eb
xyt556 <xyt556@gmail.com>
2023-02-24 10:12:30 +0800

    ReverseArray

--- ReverseArray/Program.cs
+++ ReverseArray/Program.cs
@@ -27,12 +27,24 @@ namespace ReverseArray
                 end--;
             }
         }
+        static void Reverse1(int[] arr)
+        {
+            int len = arr.Length;   //求数组的长度  for (int i = 0; i < len / 2; i++)
+            for (int i = 0; i < len / 2; i++)
+            { 
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[len - i - 1];
+                arr[len - i - 1] = temp;
+            }
+        }
+
         static void Main(string[] args)
         {
             int[] arr = { 1, 2, 3, 4, 5 };
             Console.WriteLine("数组初值：");
             PrintArr(arr); 
-            Reverse(arr);
+            Reverse1(arr);
             Console.WriteLine("逆置后的数组值：");
             PrintArr(arr);
 
